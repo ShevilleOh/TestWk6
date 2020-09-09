a408ad33ffa7c635fbc38d95cea38a4378067868
OH <ohmydaniel@gmail.com>
2020-09-10 04:16:30 +0900

    fix wrong register path

--- src/component/auth/Register.js
+++ src/component/auth/Register.js
@@ -1,124 +1,85 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 
-import placeholder from './../../img/placeholder.png';
-import placeholder2 from './../../img/placeholder2.png';
-import placeholder3 from './../../img/placeholder3.png';
-import placeholder4 from './../../img/placeholder4.jpg';
+const Register = props => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    })
 
-const Browse = () => {
+    const { name, email, password, password2} = formData;
+
+    const handleChange = text => e => {
+        setFormData({ ...formData, [text]: e.target.value });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log("button clicked");
+        console.log(name, email, password);
+    };
+
     return (
-        <div className="browse">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12 text-center">
-                        <h1 className="display-4 text-center">
-                            {' '}
-                            Browse to your heart's content!
-                        </h1>
-                        <p/>
-                        <div className="container browse-outer">
-                            <div className="container browse-inner">
-                                <div className="container browse-profile-picture">
-                                    <img src={placeholder} class="rounded float-left" aria-hidden alt="description of image"/>
-                                </div>
-                                <div className="container browse-profile-summary">
-                                    <h1 className="browse-name">
-                                        Anuja Nautreel
-                                    </h1>
-                                    <p className="browse-details">
-                                        Lead 3D Artist @ UnrealVirtualGames
-                                    </p>
-                                    <p className="browse-details">
-                                        Master of Animation @ RMIT
-                                    </p>
-                                    <p className="browse-details">
-                                        Bachelor of Design @ University of Melbourne
-                                    </p>
-                                    <Link to="/profile" className={"btn btn-lg btn-info mr-2"}>
-                                        Browse
-                                    </Link>
-                                    <Link to={"/login"} className={"btn btn-lg btn-light"}>
-                                        Slide into her DM
-                                    </Link>
-                                </div>
+        <div className={"register"}>
+            <div className={"container"}>
+                <div className={"row"}>
+                    <div className="col-mid-8 m-auto">
+                        <h1 className="display-4 text-center">Sign Up</h1>
+                        <p className={"lead text-center"}>
+                            Create your EliteConnector account
+                        </p>
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <input
+                                    type={"text"}
+                                    className={"form-control form-control-lg"}
+                                    placeholder="User Name"
+                                    name={"name"}
+                                    value={name}
+                                    onChange={handleChange( 'name')}
+                                />
                             </div>
-                            <div className="container browse-inner">
-                                <div className="container browse-profile-picture">
-                                    <img src={placeholder2} class="rounded float-left" aria-hidden alt="description of image"/>
-                                </div>
-                                <div className="container browse-profile-summary">
-                                    <h1 className="browse-name">
-                                        John Smith
-                                    </h1>
-                                    <p className="browse-details">
-                                        Machine Learning Engineer @ UnrealMachineLearning
-                                    </p>
-                                    <p className="browse-details">
-                                        Master of IT @ La Trobe University
-                                    </p>
-                                    <p className="browse-details">
-                                        Bachelor of Science @ University of Melbourne
-                                    </p>
-                                    <Link to="/profile2" className={"btn btn-lg btn-info mr-2"}>
-                                        Browse
-                                    </Link>
-                                    <Link to={"/login"} className={"btn btn-lg btn-light"}>
-                                        Slide into his DM
-                                    </Link>
-                                </div>
+                            <div className="form-group">
+                                <input
+                                    type={"email"}
+                                    className={"form-control form-control-lg"}
+                                    placeholder="Email"
+                                    name={"email"}
+                                    value={email}
+                                    onChange={handleChange ('email')}
+                                />
+                                <small className="form-text text-muted">
+                                    This site uses Gravatar so if you want a profile image,
+                                    use a Gravatar email
+                                </small>
                             </div>
-                            <div className="container browse-inner">
-                                <div className="container browse-profile-picture">
-                                    <img src={placeholder3} class="rounded float-left" aria-hidden alt="description of image"/>
-                                </div>
-                                <div className="container browse-profile-summary">
-                                    <h1 className="browse-name">
-                                        Bruce Wayne
-                                    </h1>
-                                    <p className="browse-details">
-                                        CEO @ Wayne Enterprises
-                                    </p>
-                                    <p className="browse-details">
-                                        Drop-out @ League of Assassins
-                                    </p>
-                                    <p className="browse-details">
-                                        Master ninja @ League of Assassins
-                                    </p>
-                                    <Link to="/profile3" className={"btn btn-lg btn-info mr-2"}>
-                                        Browse
-                                    </Link>
-                                    <Link to={"/login"} className={"btn btn-lg btn-light"}>
-                                        Turn on Bat Signal
-                                    </Link>
-                                </div>
+                            <div className="form-group">
+                                <input
+                                    type={"password"}
+                                    className={"form-control form-control-lg"}
+                                    placeholder="Password"
+                                    name={"password"}
+                                    value={password}
+                                    onChange={handleChange( 'password')}
+                                />
                             </div>
-                            <div className="container browse-inner">
-                                <div className="container browse-profile-picture">
-                                    <img src={placeholder4} class="rounded float-left" aria-hidden alt="description of image"/>
-                                </div>
-                                <div className="container browse-profile-summary">
-                                    <h1 className="browse-name">
-                                        Билли Айлиш
-                                    </h1>
-                                    <p className="browse-details">
-                                        CEO @ Ctrl Alt Elite
-                                    </p>
-                                    <p className="browse-details">
-                                        Master of IT @ University of Melbourne
-                                    </p>
-                                    <p className="browse-details">
-                                        Bachelor of Science @ University of Melbourne
-                                    </p>
-                                    <Link to="/profile4" className={"btn btn-lg btn-info mr-2"}>
-                                        Browse
-                                    </Link>
-                                    <Link to={"/login"} className={"btn btn-lg btn-light"}>
-                                        Slide into her DM
-                                    </Link>
-                                </div>
+                            <div className="form-group">
+                                <input
+                                    type={"password"}
+                                    className={"form-control form-control-lg"}
+                                    placeholder="Confirm Password"
+                                    name="password2"
+                                    value={password2}
+                                    onChange={handleChange( 'password2')}
+                                />
                             </div>
-                        </div>
+                            <input
+                                type="submit"
+                                className="btn btn-info btn-block mt-4"
+                            />
+                        </form>
                     </div>
                 </div>
             </div>
@@ -126,4 +87,4 @@ const Browse = () => {
     );
 };
 
-export default Browse;
+export default Register;
